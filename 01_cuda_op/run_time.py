e8fe11f5fbcbca0d95fc7a39156938f9613ab4b1
ifromeast <dongsjtu@163.com>
2023-09-03 12:14:09 +0800

    matadd cuda op

--- 01_cuda_op/run_time.py
+++ 01_cuda_op/run_time.py
@@ -74,5 +74,7 @@ if __name__ == "__main__":
     cuda_time, cuda_res = show_time(run_cuda)
     print("Cuda time:  {:.3f}us".format(np.mean(cuda_time)))
 
+    print(cuda_res, cuda_res.shape)
+    print(torch_res, torch_res.shape)
     print(torch.allclose(cuda_res, torch_res))
     print("Kernel test passed.")
